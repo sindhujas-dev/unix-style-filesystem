fa0e7d39ed8f28a0b2b2cc7e10dfac5dda054a2a
Sindhuja Sarabu <sindhuja.sarabu@stonybrook.edu>
2025-04-12 02:32:04 +0000

    read function done

--- src/inode_manip.c
+++ src/inode_manip.c
@@ -8,6 +8,8 @@
 
 #include <math.h>
 
+#include <stdbool.h>
+
 #define INDIRECT_DBLOCK_INDEX_COUNT (DATA_BLOCK_SIZE / sizeof(dblock_index_t) - 1)
 #define INDIRECT_DBLOCK_MAX_DATA_SIZE ( DATA_BLOCK_SIZE * INDIRECT_DBLOCK_INDEX_COUNT )
 
@@ -227,10 +229,147 @@ fs_retcode_t write_data_in_indirect_dblock(filesystem_t *fs, inode_t *inode, voi
     return SUCCESS;
 }
 
+//helper function made to reach the certain dblock we should work with, given an offset in bytes
+fs_retcode_t find_dblock_with_bytes(filesystem_t *fs, inode_t *inode, size_t offset, byte **dblock_ptr, size_t *offset_within_dblock_ptr, bool need_to_write){
+    if(fs == NULL || inode == NULL || dblock_ptr == NULL || offset_within_dblock_ptr == NULL){
+        return INVALID_INPUT;
+    }
+
+    //if the offset in bytes is bigger than the file size in bytes, and we are not writing data, throw error
+    //not possible to read after file size
+    if(offset >= inode->internal.file_size && need_to_write == false){
+        return INVALID_INPUT;
+    }
+
+    //dblock_ptr keeps track of the dblock that we want to return
+    //offset_within_dblock_ptr keep track of how many bytes are already in the dblock, and what we should do next
+    *dblock_ptr = NULL;
+    *offset_within_dblock_ptr = 0;
+
+    //check if the offset is within the direct D-blocks
+    if(offset < INODE_DIRECT_BLOCK_COUNT * DATA_BLOCK_SIZE){
+        // find the last claimed dblock
+        size_t dblock_index = offset / DATA_BLOCK_SIZE;
+
+        // find the offset in the dblock to write to
+        *offset_within_dblock_ptr = offset % DATA_BLOCK_SIZE;
+
+        // check if dblock is allocated
+        if(inode->internal.direct_data[dblock_index] == 0){
+            //if we are only reading, throw error
+            if(need_to_write == false){
+                return INVALID_INPUT;
+            }
+
+            dblock_index_t new_dblock_index;
+            fs_retcode_t new_dblock_return = claim_available_dblock(fs, &new_dblock_index);
+            if(new_dblock_return  != SUCCESS){
+                return INSUFFICIENT_DBLOCKS;
+            }
+            inode->internal.direct_data[dblock_index] = new_dblock_index;
+        }
+
+        // set the output pointer to the start of the dblock
+        *dblock_ptr = fs->dblocks + (inode->internal.direct_data[dblock_index] * DATA_BLOCK_SIZE);
+        return SUCCESS;
+    }
+
+    //the offset is not in direct dblocks, so check indirect dblocks
+
+    //check if the first indirect dblock is not allocated
+    if(inode->internal.indirect_dblock == 0){
+        //if we are only reading, then throw error
+        if(need_to_write == false){
+            return INVALID_INPUT;
+        }
+
+        //if we are writing, need to allocate new indirect dblock
+        dblock_index_t new_indirect_dblock_index;
+        fs_retcode_t new_dblock_return = claim_available_dblock(fs, &new_indirect_dblock_index);
+        if(new_dblock_return  != SUCCESS){
+            return INSUFFICIENT_DBLOCKS;
+        }
+        memset(fs->dblocks + (new_indirect_dblock_index * DATA_BLOCK_SIZE), 0, DATA_BLOCK_SIZE);
+        inode->internal.indirect_dblock = new_indirect_dblock_index;
+    }
+
+    //find the position within the indirect dblocks
+
+    //find the offset after removing the bytes within the direct dblocks
+    size_t indirect_offset = offset - (INODE_DIRECT_BLOCK_COUNT * DATA_BLOCK_SIZE);
+
+    //find the index dblock number that we are working with
+    size_t curr_index_block_number = indirect_offset / INDIRECT_DBLOCK_MAX_DATA_SIZE;
+
+    //find the byte offset within the current index block's range of data
+    size_t offset_within_index_block = indirect_offset % INDIRECT_DBLOCK_MAX_DATA_SIZE;
+
+    //find the data block number within the current index block 
+    size_t data_block_index_in_current_index = offset_within_index_block / DATA_BLOCK_SIZE;
+
+    //calculates the byte offset within the specific data dblock
+    *offset_within_dblock_ptr = offset_within_index_block % DATA_BLOCK_SIZE;
+
+    //start with the first indirect dblock
+    dblock_index_t curr_indirect_dblock_index = inode->internal.indirect_dblock;
+
+    // go through all the index dblocks and find the last one we use
+    for(size_t i = 0; i < curr_index_block_number; i++){
+        //get a pointer to the current indirect index dblock
+        dblock_index_t *curr_indirect_dblock_index_ptr = cast_dblock_ptr(fs->dblocks + (curr_indirect_dblock_index * DATA_BLOCK_SIZE));
+
+        //if the next index dblock is not allocated
+        dblock_index_t next_index_dblock = curr_indirect_dblock_index_ptr[15];
+        if(next_index_dblock == 0){
+            //if we need to just read, throw error
+            if(need_to_write == false){
+                return INVALID_INPUT;
+            }
+
+            //if we need to write, create new indirect dblock
+            dblock_index_t new_indirect_dblock_index;
+            fs_retcode_t new_dblock_return = claim_available_dblock(fs, &new_indirect_dblock_index);
+            if(new_dblock_return != SUCCESS){
+                return INSUFFICIENT_DBLOCKS;
+            }
+            memset(fs->dblocks + (new_indirect_dblock_index * DATA_BLOCK_SIZE), 0, DATA_BLOCK_SIZE);
+            curr_indirect_dblock_index_ptr[15] = new_indirect_dblock_index;
+            curr_indirect_dblock_index = new_indirect_dblock_index;
+        }else{
+            curr_indirect_dblock_index = next_index_dblock;
+        }
+    }
+
+    // now the current indirect dblock index points to the last index dblock used
+    dblock_index_t *curr_indirect_dblock_index_ptr = cast_dblock_ptr(fs->dblocks + (curr_indirect_dblock_index * DATA_BLOCK_SIZE));
+
+    //check if the data dblock is allocated
+    if(curr_indirect_dblock_index_ptr[data_block_index_in_current_index] == 0){
+        if(need_to_write == false){
+            return INVALID_INPUT;
+        }
+
+        //to write, allocate a new dblock
+        dblock_index_t new_data_dblock_index;
+        fs_retcode_t result = claim_available_dblock(fs, &new_data_dblock_index);
+        if(result != SUCCESS){
+            return INSUFFICIENT_DBLOCKS;
+        }
+
+        //update hte index dblock with the new data dblock index
+        curr_indirect_dblock_index_ptr[data_block_index_in_current_index] = new_data_dblock_index;
+    }
+
+    //set the output pointer to the start of the data dblock
+    *dblock_ptr = fs->dblocks + (curr_indirect_dblock_index_ptr[data_block_index_in_current_index] * DATA_BLOCK_SIZE);
+
+    return SUCCESS;
+}
+
+
 // ----------------------- CORE FUNCTION ----------------------- //
 
-fs_retcode_t inode_write_data(filesystem_t *fs, inode_t *inode, void *data, size_t n)
-{
+fs_retcode_t inode_write_data(filesystem_t *fs, inode_t *inode, void *data, size_t n){
     //Check for valid input
     if(fs == NULL || inode == NULL){
         return INVALID_INPUT;
@@ -267,15 +406,61 @@ fs_retcode_t inode_write_data(filesystem_t *fs, inode_t *inode, void *data, size
 
 fs_retcode_t inode_read_data(filesystem_t *fs, inode_t *inode, size_t offset, void *buffer, size_t n, size_t *bytes_read)
 {
-    (void)fs;
-    (void)inode;
-    (void)offset;
-    (void)buffer;
-    (void)n;
-    (void)bytes_read;
-    return NOT_IMPLEMENTED;
-    
     //check to make sure inputs are valid
+    if(fs == NULL || inode == NULL || bytes_read == NULL){
+        return INVALID_INPUT;
+    }
+
+    *bytes_read = 0;
+
+    //get current file size
+    size_t current_file_size = inode->internal.file_size;
+
+    //check if the offset is larger than the file size, nothign to read past file size
+    if(offset >= current_file_size){
+        return SUCCESS;
+    }
+
+    //start reading at offset
+    size_t file_bytes_to_read = current_file_size - offset;
+    if(n > file_bytes_to_read){
+        n = file_bytes_to_read;
+    }
+
+    //if offset equaled current file size
+    if(n == 0){
+        return SUCCESS;
+    }
+
+    //need to access memory through a byte pointer to increment by bytes, and read data
+    byte *buffer_destination = (byte *)buffer;
+    size_t remaining_bytes_to_read = n;
+    size_t current_offset = offset;
+
+    //until we read all the bytes
+    while(remaining_bytes_to_read > 0){
+        byte *dblock_ptr;
+        size_t offset_within_dblock;
+        fs_retcode_t result = find_dblock_with_bytes(fs, inode, current_offset, &dblock_ptr, &offset_within_dblock, false);
+        if(result != SUCCESS){ //if some error occurs just return success
+            return SUCCESS;
+        }
+        //calculate how many bytes we can read from this dblock
+        size_t curr_bytes_in_dblock = DATA_BLOCK_SIZE - offset_within_dblock;
+        if(curr_bytes_in_dblock > remaining_bytes_to_read){
+            curr_bytes_in_dblock = remaining_bytes_to_read;
+        }
+
+        //copy data into the buffer destination
+        memcpy(buffer_destination, dblock_ptr + offset_within_dblock, curr_bytes_in_dblock);
+
+        buffer_destination += curr_bytes_in_dblock;
+        current_offset += curr_bytes_in_dblock;
+        remaining_bytes_to_read -= curr_bytes_in_dblock;
+        *bytes_read += curr_bytes_in_dblock;
+
+    }
+    return SUCCESS;
 
     //for 0 to n, use the helper function to read and copy 1 byte at a time
 }
@@ -302,9 +487,6 @@ fs_retcode_t inode_modify_data(filesystem_t *fs, inode_t *inode, size_t offset, 
 
 fs_retcode_t inode_shrink_data(filesystem_t *fs, inode_t *inode, size_t new_size)
 {
-    //display_filesystem(fs, DISPLAY_FS_FORMAT);
-    //display_filesystem(fs, DISPLAY_INODES);
-    
     //check to see if inputs are in valid range
     if(fs == NULL || inode == NULL){
         return INVALID_INPUT;
@@ -319,7 +501,7 @@ fs_retcode_t inode_shrink_data(filesystem_t *fs, inode_t *inode, size_t new_size
     if(new_size == inode->internal.file_size){
         return SUCCESS;
     }
-
+    
     return SUCCESS;
 }
 
@@ -329,22 +511,5 @@ fs_retcode_t inode_release_data(filesystem_t *fs, inode_t *inode)
     if(fs == NULL || inode == NULL){
         return INVALID_INPUT;
     }
-    // for(int i = 0; i < INODE_DIRECT_BLOCK_COUNT; i++){
-    //     //info(1, "current loop index: %d\n", i);
-
-    //     dblock_index_t direct_dblock_index = inode->internal.direct_data[i];
-    //     //info(1, "current dblock index: %u\n", direct_dblock_index);
-
-    //     if(direct_dblock_index != 0){
-    //         //info(1, "Releasing dblock with index: %zu\n", direct_dblock_index);
-    //         //find the memory address of the specific block, using the dblock index
-    //         byte *dblock_ptr = fs->dblocks + (direct_dblock_index * DATA_BLOCK_SIZE);
-    //         fs_retcode_t returnCode = release_dblock(fs, dblock_ptr);
-    //         if(returnCode != SUCCESS){
-    //             return returnCode;
-    //         }
-    //         inode->internal.direct_data[i] = 0;
-    //     }
-    // }
     return SUCCESS;
 }
